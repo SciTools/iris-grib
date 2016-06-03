57e29f7d065cc4a93d258c044b4831c9bde27a22
Bill Little <bill.james.little@gmail.com>
2016-06-03 14:32:48 +0100

    wip

--- iris_grib/tests/unit/test_GribWrapper.py
+++ iris_grib/tests/unit/test_GribWrapper.py
@@ -22,7 +22,7 @@ Unit tests for the `iris_grib.GribWrapper` class.
 from __future__ import (absolute_import, division, print_function)
 from six.moves import (filter, input, map, range, zip)  # noqa
 
-# Import iris.tests first so that some things can be initialised before
+# Import iris_grib.tests first so that some things can be initialised before
 # importing anything else.
 import iris_grib.tests as tests
 
