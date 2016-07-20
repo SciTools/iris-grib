69924454caea0900461168f9868d6dd765c05543
marqh <markh@metarelate.net>
2016-07-20 12:35:53 +0000

    Update __init__.py
    bump dev version label

--- iris_grib/__init__.py
+++ iris_grib/__init__.py
@@ -47,7 +47,7 @@ from ._load_convert import convert as load_convert
 from .message import GribMessage
 
 
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0.dev1'
 
 __all__ = ['load_cubes', 'save_grib2', 'load_pairs_from_fields',
            'save_pairs_from_cube', 'save_messages', 'GribWrapper']
