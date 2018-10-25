81d96b400a16db4b388a3db5435567f97a1e9bf0
Corinne Bosley <corinne.e.bosley@gmail.com>
2018-10-25 12:47:43 +0100

    added skeleton test file for save rules

---
+++ iris_grib/tests/unit/save_rules/test_product_definition_template_15.py
@@ -0,0 +1,36 @@
+# (C) British Crown Copyright 2018, Met Office
+#
+# This file is part of iris-grib.
+#
+# iris-grib is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# iris-grib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with iris-grib.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Unit tests for :func:`iris_grib._save_rules.product_definition_template_11`
+
+"""
+
+from __future__ import (absolute_import, division, print_function)
+from six.moves import (filter, input, map, range, zip)  # noqa
+
+# Import iris_grib.tests first so that some things can be initialised before
+# importing anything else.
+import iris_grib.tests as tests
+
+from cf_units import Unit
+import gribapi
+import mock
+
+from iris.coords import CellMethod, DimCoord
+import iris.tests.stock as stock
+
+from iris_grib._save_rules import product_definition_template_15
