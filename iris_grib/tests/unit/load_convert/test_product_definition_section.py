0ecc2b87529010c8fdd7d3dafea891c38bcbe67f
trexfeathers <martin_yeo@new-forest.org>
2019-06-06 17:18:49 +0100

    Finished test_product_definition_section.py (needs commenting) Made section_4 importable from test_product_definition_template_31.py

--- iris_grib/tests/unit/load_convert/test_product_definition_section.py
+++ iris_grib/tests/unit/load_convert/test_product_definition_section.py
@@ -19,12 +19,6 @@ Tests for function :func:`iris_grib._load_convert.product_definition_section`.
 
 """
 
-# QUESTIONS
-# tests here will overlap with individual template tests, as they are all
-#   called within product_definition_section. Is this a problem?
-# a lot of tests have unused imports, seemingly because they were copied from
-#   other tests. Is this standard practice?
-
 from __future__ import (absolute_import, division, print_function)
 from six.moves import (filter, input, map, range, zip)  # noqa
 
@@ -39,20 +33,88 @@ from cf_units import Unit
 from iris.coords import DimCoord
 
 from iris_grib._load_convert import product_definition_section
+from iris_grib._load_convert import vertical_coords
 from iris_grib.grib_phenom_translation import _GribToCfDataClass
 
 from iris_grib.tests.unit.load_convert import empty_metadata
 from iris_grib.tests.unit.load_convert.test_product_definition_template_0 \
     import section_4 as template_0
+from iris_grib.tests.unit.load_convert.test_product_definition_template_31 \
+    import section_4 as template_31
+
 
 class Test(tests.IrisGribTest):
-    def test_fixed_surface_present(self):
-        rt_coord = mock.sentinel.observation_time
+    def setUp(self):
+        self.patch('warnings.warn')
+        # self.product_definition_section_patch = self.patch(
+        #     'iris_grib._load_convert.product_definition_section'
+        # )
+        # self.vertical_coords_patch = self.patch(
+        #     'iris_grib._load_convert.vertical_coords'
+        # )
+        self.translate_phenomenon_patch = self.patch(
+            'iris_grib._load_convert.translate_phenomenon'
+        )
+
+
+class TestFixedSurfaces(Test):
+    def generic_fixed_surface_test(self, fs_is_expected, fs_is_present):
+        discipline = mock.sentinel.discipline
+        tablesVersion = mock.sentinel.tablesVersion
+        rt_coord = DimCoord(24, 'forecast_reference_time',
+                                        units='hours since epoch')
         metadata = empty_metadata()
-        section_4 = template_0()
-        product_definition_section(section_4, metadata, rt_coord)
 
+        templates = {0: template_0(), 31: template_31()}
+        template_number = 0 if fs_is_expected else 31
+        section_4 = templates[template_number]
+        section_4.update({
+            'productDefinitionTemplateNumber': template_number,
+            'parameterCategory': None,
+            'parameterNumber': None
+        })
 
+        fixed_surface_keys = [
+            'typeOfFirstFixedSurface',
+            'scaledValueOfFirstFixedSurface',
+            'typeOfSecondFixedSurface'
+        ]
+        for key in fixed_surface_keys:
+            if fs_is_present and key not in section_4:
+                section_4[key] = template_0()[key]
+            elif (not fs_is_present) and key in section_4:
+                del section_4[key]
 
-    # def test_fixed_surface_absent:
+        def run_function():
+            product_definition_section(
+                section_4, metadata, discipline, tablesVersion, rt_coord)
 
+        if fs_is_expected and not fs_is_present:
+            with self.assertRaises(KeyError) as context:
+                run_function()
+            self.assertTrue('FixedSurface' in str(context.exception))
+        else:
+            run_function()
+            self.assertEqual(self.translate_phenomenon_patch.call_count, 1)
+            phenom_call_args = self.translate_phenomenon_patch.call_args[1]
+            for key in fixed_surface_keys:
+                if fs_is_expected:
+                    self.assertEqual(phenom_call_args[key], section_4[key])
+                else:
+                    self.assertEqual(phenom_call_args[key], None)
+
+    def test_fixed_surface_expected_present(self):
+        self.generic_fixed_surface_test(fs_is_expected=True,
+                                        fs_is_present=True)
+
+    def test_fixed_surface_expected_absent(self):
+        self.generic_fixed_surface_test(fs_is_expected=True,
+                                        fs_is_present=False)
+
+    def test_fixed_surface_ignored_present(self):
+        self.generic_fixed_surface_test(fs_is_expected=False,
+                                        fs_is_present=True)
+
+    def test_fixed_surface_ignored_absent(self):
+        self.generic_fixed_surface_test(fs_is_expected=False,
+                                        fs_is_present=False)
