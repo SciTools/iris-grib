8181dbbaccec9ca29127e0597ad479f4fbbac5f5
Tom Powell <thomas.powell@metoffice.gov.uk>
2020-01-24 15:56:42 +0000

    add unit test

--- iris_grib/tests/unit/load_convert/test_grid_definition_template_140.py
+++ iris_grib/tests/unit/load_convert/test_grid_definition_template_140.py
@@ -42,7 +42,7 @@ class Test(tests.IrisGribTest):
             'longitudeOfFirstGridPoint': -4027984,
             'standardParallelInMicrodegrees' : 54900000,
             'centralLongitudeInMicrodegrees' : -2500000,
-            'resolutionAndComponentFlags': 0b00000000,
+            'resolutionAndComponentFlags': 0b00110000,
             'Dx': 2000000,
             'Dy': 2000000,
             'scanningMode': 0b01000000,
@@ -51,10 +51,40 @@ class Test(tests.IrisGribTest):
 
     def expected(self, y_dim, x_dim):
         # Prepare the expectation.
-        # TODO
+        expected = empty_metadata()
+        ellipsoid = iris.coord_systems.GeogCS(6378137,
+                                          inverse_flattening=298.257222101)
+        cs = iris.coord_systems.LambertAzimutalEqualArea(
+            latitude_of_projection_origin=54.9,
+            longitude_of_projection_origin=-2.5,
+            false_easting=0,
+            false_northing=0,
+            ellipsoid=ellipsoid)
+        lon0 = -4027984 * 1e-6
+        lat0 = 53988880 * 1e-6
+        x0m, y0m = cs.as_cartopy_crs().transform_point(
+            lon0, lat0, ccrs.Geodetic())
+        dxm = dym = 2000000.
+        x_points = x0m + dxm * np.arange(15)
+        y_points = y0m + dym * np.arange(10)
+        x = iris.coords.DimCoord(x_points,
+                                 standard_name='projection_x_coordinate',
+                                 units='m',
+                                 coord_system=cs)
+        y = iris.coords.DimCoord(y_points,
+                                 standard_name='projection_y_coordinate',
+                                 units='m',
+                                 coord_system=cs)
+        expected['dim_coords_and_dims'].append((y, y_dim))
+        expected['dim_coords_and_dims'].append((x, x_dim))
+        return expected
 
     def test(self):
-        # TODO
+        section = self.section_3()
+        metadata = empty_metadata()
+        grid_definition_template_140(section, metadata)
+        expected = self.expected(0, 1)
+        self.assertEqual(metadata, expected)
 
 
 if __name__ == '__main__':
