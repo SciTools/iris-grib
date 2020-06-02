2193f5263318214cc5ef3eda8316a9d5d259f785
lbdreyer <lbdreyer@users.noreply.github.com>
2020-06-02 17:37:50 +0100

    Latest eccodes + workaround seg fault when saving (#208)

--- iris_grib/_save_rules.py
+++ iris_grib/_save_rules.py
@@ -1489,6 +1489,13 @@ def data_section(cube, grib):
         # Enable missing values in the grib message.
         gribapi.grib_set(grib, "bitmapPresent", 1)
         gribapi.grib_set_double(grib, "missingValue", fill_value)
+
+    # A segmentation fault is raised by `gribapi.grib_set_double_array` if it
+    # tries to cast large data to float64. As a temporary fix we cast the data
+    # upfront
+    # TODO: remove the `astype` command once eccodes (gribapi) has been fixed.
+    if data.dtype != np.float64:
+        data = data.astype(np.float64)
     gribapi.grib_set_double_array(grib, "values", data.flatten())
 
     # todo: check packing accuracy?
