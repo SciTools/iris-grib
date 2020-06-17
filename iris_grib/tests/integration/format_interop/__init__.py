c85708e6eafa3d33245af335d3e206ec4adacd39
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-06-17 15:38:55 +0100

    Tests for saving other-format data into grib : ported from Iris.

---
+++ iris_grib/tests/integration/format_interop/__init__.py
@@ -0,0 +1,12 @@
+# Copyright iris-grib contributors
+#
+# This file is part of iris-grib and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+"""
+Integration tests for format interoperability.
+
+Checks that specific features of cubes loaded from other formats are correctly
+represented by grib saving.
+
+"""
