b6a5183abe509f983fce0bdaba6c91d51bbb82a7
Martin Yeo <martin.yeo@metoffice.gov.uk>
2020-09-29 15:36:57 +0100

    PyPi improvements in line with SciTools/iris#3812.

--- setup.py
+++ setup.py
@@ -2,46 +2,57 @@
 
 import os
 import os.path
-import sys
 from setuptools import setup
-import textwrap
 
 
-name = 'iris_grib'
-
-
-LONG_DESCRIPTION = textwrap.dedent("""
-    Iris loading of GRIB files
-    ==========================
-
-    With this package, iris is able to load GRIB files:
-
-    ```
-    my_data = iris.load(path_to_grib_file)
-    ```
-    """)
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-pkg_root = os.path.join(here, name)
+NAME = 'iris_grib'
+PYPI_NAME = 'iris-grib'
+PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+PACKAGE_ROOT = os.path.join(PACKAGE_DIR, NAME)
 
 packages = []
-for d, _, _ in os.walk(os.path.join(here, name)):
+for d, _, _ in os.walk(os.path.join(PACKAGE_DIR, NAME)):
     if os.path.exists(os.path.join(d, '__init__.py')):
-        packages.append(d[len(here)+1:].replace(os.path.sep, '.'))
+        packages.append(d[len(PACKAGE_DIR) + 1:].replace(os.path.sep, '.'))
+
+
+def pip_requirements(*args):
+    requirements = []
+    for name in args:
+        fname = os.path.join(
+            PACKAGE_DIR, "requirements", "{}.txt".format(name)
+        )
+        if not os.path.exists(fname):
+            emsg = (
+                f"Unable to find the {name!r} requirements file at {fname!r}"
+            )
+            raise RuntimeError(emsg)
+        with open(fname, "r") as fh:
+            for line in fh:
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                requirements.append(line)
+    return requirements
 
 
 def extract_version():
     version = None
-    fdir = os.path.dirname(__file__)
-    fnme = os.path.join(fdir, 'iris_grib', '__init__.py')
-    with open(fnme) as fd:
-        for line in fd:
+    fname = os.path.join(PACKAGE_DIR, 'iris_grib', '__init__.py')
+    with open(fname) as fi:
+        for line in fi:
             if (line.startswith('__version__')):
                 _, version = line.split('=')
-                version = version.strip()[1:-1]  # Remove quotation characters
+                version = version.strip()[1:-1]  # Remove quotations
                 break
     return version
+
+
+def long_description():
+    fname = os.path.join(PACKAGE_DIR, "README.md")
+    with open(fname, "rb") as fi:
+        result = fi.read().decode("utf-8")
+    return result
 
 
 def file_walk_relative(top, remove=''):
@@ -58,13 +69,14 @@ def file_walk_relative(top, remove=''):
 
 
 setup_args = dict(
-    name             = name,
+    name             = PYPI_NAME,
     version          = extract_version(),
     packages         = packages,
     package_data     = {'iris_grib': list(file_walk_relative('iris_grib/tests/results',
                                           remove='iris_grib/'))},
     description      = "GRIB loading for Iris",
-    long_description = LONG_DESCRIPTION,
+    long_description = long_description(),
+    long_description_content_type = "text/markdown",
     url              = 'https://github.com/SciTools/iris-grib',
     author           = 'UK Met Office',
     author_email     = 'scitools-iris@googlegroups.com',
@@ -78,10 +90,14 @@ setup_args = dict(
         'Programming Language :: Python :: 3 :: Only',
     ],
     # NOTE: The Python 3 bindings to eccodes (eccodes-python) is available on
-    # PyPI, but the user is required to install eccodes itself manually. See 
+    # PyPI, but the user is required to install eccodes itself manually. See
     # ECMWF ecCodes installation documentation for more information.
-    install_requires=['scitools-iris>=2.4.*'] + ['eccodes-python'],
-    test_suite = 'iris_grib.tests',
+    install_requires=pip_requirements("core"),
+    test_suite = f'{PYPI_NAME}.tests',
+    extras_require={
+        "all": pip_requirements("all"),
+        "test": pip_requirements("test"),
+    },
 )
 
 
