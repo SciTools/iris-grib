c536e49f387c246a180adaa1abfd0849af00265d
Tom Dufall <tom@tomdufall.co.uk>
2021-06-01 14:29:35 +0100

    Remove test for removed functionality

--- iris_grib/tests/unit/message/test__DataProxy.py
+++ iris_grib/tests/unit/message/test__DataProxy.py
@@ -43,31 +43,5 @@ class Test__bitmap(tests.IrisGribTest):
             data_proxy._bitmap(section_6)
 
 
-class Test_emptyfetch(tests.IrisGribTest):
-    # See :
-    #   iris.tests.unit.fileformats.pp.test_PPDataProxy.Test__getitem__slicing
-    # In this case, test *only* the no-data-read effect, not the method which
-    # is part of Iris.
-    def test_empty_slice(self):
-        # Check behaviour of the getitem call with an 'empty' slicing.
-        # This is necessary because, since Dask 2.0, the "from_array" function
-        # takes a zero-length slice of its array argument, to capture array
-        # metadata, and in those cases we want to avoid file access.
-        test_dtype = np.dtype(np.float32)
-        mock_datafetch = mock.MagicMock()
-        proxy = _DataProxy(shape=(3, 4),
-                           dtype=np.dtype(np.float32),
-                           recreate_raw=mock_datafetch)
-
-        # Test the special no-data indexing operation.
-        result = proxy[0:0, 0:0]
-
-        # Check the behaviour and results were as expected.
-        self.assertEqual(mock_datafetch.call_count, 0)
-        self.assertIsInstance(result, np.ndarray)
-        self.assertEqual(result.dtype, test_dtype)
-        self.assertEqual(result.shape, (0, 0))
-
-
 if __name__ == '__main__':
     tests.main()
