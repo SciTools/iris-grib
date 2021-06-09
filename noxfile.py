657e6ccf6e98e2f8bb5192621c2cf503472d4e13
tomdufall <tom@tomdufall.co.uk>
2021-06-09 20:15:47 +0000

    Revert "Fix nox when HOME not set"
    This reverts commit 3fddc50c2ad622ee210e135816e0ede55195c6c7.

--- noxfile.py
+++ noxfile.py
@@ -29,10 +29,7 @@ PY_VER = os.environ.get("PY_VER", ["3.7", "3.8"])
 IRIS_SOURCE = os.environ.get("IRIS_SOURCE", ['source', 'conda-forge'])
 
 #: Default cartopy cache directory.
-if os.getenv("HOME"):
-    CARTOPY_CACHE_DIR = Path(os.getenv("HOME")) / ".local/share/cartopy"
-else:
-    CARTOPY_CACHE_DIR = None
+CARTOPY_CACHE_DIR = os.environ.get("HOME") / Path(".local/share/cartopy")
 
 
 def _cache_cartopy(session: nox.sessions.Session) -> None:
