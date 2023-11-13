897a92f3da7ba9e4b32d569332986e315143cf38
lbdreyer <laura.dreyer@metoffice.gov.uk>
2023-11-13 11:18:48 +0000

    Relicense from LGPL-3 to BSD-3

--- iris_grib/tests/unit/save_rules/test_fixup_int32_as_uint32.py
+++ iris_grib/tests/unit/save_rules/test_fixup_int32_as_uint32.py
@@ -1,8 +1,7 @@
 # Copyright iris-grib contributors
 #
-# This file is part of iris-grib and is released under the LGPL license.
-# See COPYING and COPYING.LESSER in the root of the repository for full
-# licensing details.
+# This file is part of iris-grib and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
 """
 Unit tests for `iris_grib._save_rules.fixup_int32_as_uint32`.
 
