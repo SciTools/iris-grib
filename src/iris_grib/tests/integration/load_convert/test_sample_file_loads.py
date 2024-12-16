d44fdf9c505227c25cd7ee59310880f50130d99a
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2024-12-16 17:36:15 +0000

    Full test coverage for GRIB1 (#583)
    * More integration tests for GRIB1 files.
    
    * Test coverage for timeRangeIndicators.
    
    * Add Polar Stereo South coverage for GRIB1.
    
    * Add Reduced GG coverage for GRIB 1.
    
    * Add coverage for identification of y-wind in GRIB1.
    
    * Add coverage for CF-mapping in GRIB1.
    
    * Widen reduced GG GRIB1 test to cover ML level type.
    
    * Refactor test_reduced_gg_grib1 into TestBasicLoad.
    
    * Tidy new tests.

--- src/iris_grib/tests/integration/load_convert/test_sample_file_loads.py
+++ src/iris_grib/tests/integration/load_convert/test_sample_file_loads.py
@@ -10,11 +10,18 @@ reference CML file, to catch any unexpected changes over time.
 
 """
 
+from __future__ import annotations
+
 # import iris_grib.tests first so that some things can be initialised
 # before importing anything else.
 import iris_grib.tests as tests
 
+from pathlib import Path
+
+import eccodes
 import iris
+import pytest
+
 
 _RESULTDIR_PREFIX = ("integration", "load_convert", "sample_file_loads")
 
@@ -111,11 +118,32 @@ class TestBasicLoad(tests.IrisGribTest):
         )
         self.assertCML(cube, _RESULTDIR_PREFIX + ("reduced_ll_grib1.cml",))
 
-    def test_reduced_gg(self):
+    def test_reduced_gg_grib1(self):
+        cube = iris.load_cube(
+            Path(eccodes.codes_samples_path()) / "reduced_gg_ml_grib1.tmpl"
+        )
+        self.assertCML(cube, _RESULTDIR_PREFIX + ("reduced_gg_grib1.cml",))
+
+    def test_reduced_gg_grib2(self):
         cube = iris.load_cube(
             tests.get_data_path(("GRIB", "reduced", "reduced_gg.grib2"))
         )
         self.assertCML(cube, _RESULTDIR_PREFIX + ("reduced_gg_grib2.cml",))
+
+    def test_second_order_packing(self):
+        cube = iris.load_cube(
+            tests.get_data_path(
+                ("GRIB", "grib1_second_order_packing", "GRIB_00008_FRANX01")
+            )
+        )
+        self.assertCML(cube, _RESULTDIR_PREFIX + ("second_order_packing.cml",))
+
+    def test_bulletin_headers(self):
+        for byte_len in (40, 41):
+            cube = iris.load_cube(
+                tests.get_data_path(("GRIB", "bulletin", f"{byte_len}bytes.grib"))
+            )
+            self.assertCML(cube, _RESULTDIR_PREFIX + (f"bulletin_{byte_len}bytes.cml",))
 
 
 @tests.skip_data
@@ -195,6 +223,96 @@ class TestShapeOfEarth(tests.IrisGribTest):
         # grib1 - same as grib2 shape 6, above
         cube = self._old_compat_load("global.grib1")
         self.assertCML(cube, _RESULTDIR_PREFIX + ("earth_shape_grib1.cml",))
+
+
+class TestTimesGrib1:
+    # Our codebase has support for many timeRangeIndicator values in GRIB1
+    #  files, but we do not have files demonstrating these. This class
+    #  generates appropriate GRIB1 files so that we can fully refactor the
+    #  loading code while ensuring continued support for all possible
+    #  scenarios.
+
+    @pytest.fixture(params=[1, 2, 3, 4, 5, 10, 113, 118, 123, 124], autouse=True)
+    # Note that the following values are not supported by Eccodes - it can
+    #  not provide a startStep value unless the timeRangeIndicator is present
+    #  in eccodes/definitions/grib1/localConcepts/edzw/stepType.def:
+    #  [51, 114, 115, 116, 117, 125].
+    def _get_time_range_file(self, request, tmp_path):
+        save_file = tmp_path / "TestTimes.grib1"
+        time_range_indicator = request.param
+
+        # Make a file with 10 ascending time steps.
+        with save_file.open("wb") as open_file:
+            for time_step in range(10):
+                grib_message = eccodes.codes_grib_new_from_samples("GRIB1")
+                eccodes.codes_set_long(grib_message, "P1", time_step)
+                eccodes.codes_set_long(grib_message, "P2", time_step + 1)
+                eccodes.codes_set_long(
+                    grib_message, "timeRangeIndicator", time_range_indicator
+                )
+                eccodes.codes_write(grib_message, open_file)
+                eccodes.codes_release(grib_message)
+
+        self.time_range_indicator = time_range_indicator
+        self.save_file = save_file
+
+    def test_time_range(self):
+        cube = iris.load_cube(self.save_file)
+        tests.IrisGribTest().assertCML(
+            cube, _RESULTDIR_PREFIX + (f"time_range_{self.time_range_indicator}.cml",)
+        )
+
+
+class TestFullCoverageGrib1:
+    # We do not have a full set of GRIB1 files that exercise our entire GRIB1
+    #  loading code. This class generates files to cover those gaps. We use CML
+    #  testing since, in the absence of bug reports after years of use (time
+    #  of writing: 2024), we must assume that current functionality is
+    #  satisfactory. Integration tests of this sort will allow us to fully
+    #  refactor GRIB1 loading without any change in user experience.
+
+    id_path_codes = [
+        (
+            "polar_stereo_south",
+            tests.get_data_path(("GRIB", "polar_stereo", "ST4.2013052210.01h")),
+            [("projectionCentreFlag", 1)],
+        ),
+        (
+            "y_wind",
+            tests.get_data_path(("GRIB", "gaussian", "regular_gg.grib1")),
+            [("indicatorOfParameter", 34)],
+        ),
+        (
+            "mapped_cf_data",
+            Path(eccodes.codes_samples_path()) / "GRIB1.tmpl",
+            [("table2Version", 128), ("centre", 98), ("indicatorOfParameter", 34)],
+        ),
+    ]
+
+    @pytest.fixture(
+        params=id_path_codes,
+        ids=[id for id, path, codes in id_path_codes],
+        autouse=True,
+    )
+    def _get_grib1_file(self, request, tmp_path):
+        id_, path, codes = request.param
+        path_original = Path(path)
+        path_modified = tmp_path / "tmp_file.grib1"
+        with path_original.open("rb") as file_original:
+            gid = eccodes.codes_grib_new_from_file(file_original)
+            for key, value in codes:
+                eccodes.codes_set(gid, key, value)
+            with path_modified.open("wb") as file_modified:
+                eccodes.codes_write(gid, file_modified)
+            eccodes.codes_release(gid)
+        self.id_ = id_
+        self.file_path = path_modified
+
+    def test_grib1(self):
+        cube = iris.load_cube(self.file_path)
+        tests.IrisGribTest().assertCML(
+            cube, _RESULTDIR_PREFIX + (f"{self.id_}_grib1.cml",)
+        )
 
 
 if __name__ == "__main__":
