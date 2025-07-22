5cbba992df5f8315d1e93a1ca733245cec7f5c42
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2025-07-22 11:09:53 +0100

    Fix qa.

--- src/iris_grib/tests/integration/iris_integration/test_nc_attribute_handlers.py
+++ src/iris_grib/tests/integration/iris_integration/test_nc_attribute_handlers.py
@@ -8,22 +8,27 @@ Integration tests for reading/writing GRIB_PARAM attributes in netcdf files.
 N.B. the relevant support code and classes (including testing) are all part of in Iris,
 along with the ones for "STASH" and "ukmo__process_flags" ones.
 
-But all the testing there is optional, and skipped if iris-grib is not installed, so that
-the Iris repo doesn't have to maintain iris-grib as a test dependency.
+But all the testing there is optional, and skipped if iris-grib is not installed, so
+that the Iris repo doesn't have to maintain iris-grib as a test dependency.
 
 So, we import + run the relevant tests here instead.
 
 """
+
 from iris.tests.integration.netcdf.test_load_managed_attributes import (
     TestGribParam as LoadGribParamTests
 )
+
 from iris.tests.integration.netcdf.test_save_managed_attributes import (
     TestGribParam as SaveGribParamTests
 )
-from iris.tests.unit.fileformats.netcdf.attribute_handlers.test_GribParamHandler import (
-    TestEncodeObject as GribParamHandler_EncodeTests,
-    TestDecodeAttribute as GribParamHandler_DecodeTests
-)
+
+from iris.tests.unit.fileformats.netcdf.attribute_handlers.test_GribParamHandler \
+    import (
+        TestEncodeObject as GribParamHandler_EncodeTests,
+        TestDecodeAttribute as GribParamHandler_DecodeTests
+    )
+
 class TestLoadGribParam_actual(LoadGribParamTests):
     pass
 
