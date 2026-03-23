7ba78fa8b70b500a006695789de7a4a05e4582e7
Martin Yeo <martin.yeo@metoffice.gov.uk>
2026-03-23 14:28:06 +0000

    Hack to show whole structure in sidebar.

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,15 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "collapse_navigation": False,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_js_files = ["expand.js"]
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -146,10 +154,6 @@ html_theme = "sphinx_rtd_theme"
 # pixels large.
 # html_favicon = None
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
