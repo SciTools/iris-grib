5e879572cc1b935027427d52302685a3aa67e33d
Martin Yeo <martin.yeo@metoffice.gov.uk>
2026-03-24 08:58:23 +0000

    Revert "Hack to show whole structure in sidebar."
    This reverts commit 7ba78fa8b70b500a006695789de7a4a05e4582e7.

--- docs/_static/expand.js
+++
@@ -1,7 +0,0 @@
-// Leaves the sidebar permanently expanded, so the whole structure is visible.
-// https://github.com/readthedocs/sphinx_rtd_theme/issues/455#issuecomment-741734047
-window.addEventListener('load', (_event) => {
-  document.querySelectorAll(".toctree-l1").forEach(node => {
-    node.classList.add("current");
-  })
-});
