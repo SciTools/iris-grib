b1cc365f6584dd1b082041201c7291cecc1fe9d4
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2026-07-21 13:15:50 +0100

    Tidy slightly.

--- src/iris_grib/tests/unit/save_rules/test_grid_definition_template_40.py
+++ src/iris_grib/tests/unit/save_rules/test_grid_definition_template_40.py
@@ -4,8 +4,6 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for GDT 3.40 save rules."""
 
-import iris_grib.tests as tests  # noqa: F401  # required side-effect import
-
 import numpy as np
 import pytest
 
@@ -14,6 +12,10 @@ from iris.coord_systems import GeogCS
 from iris.cube import Cube
 
 import iris_grib._save_rules as save_rules
+from iris_grib._save_rules import (
+    grid_definition_template_40,
+    is_grid_definition_template_40,
+)
 
 
 def make_gaussian_cube(x_points=None, y_points=None, grib_grid_template=40):
@@ -65,26 +67,26 @@ def patched_eccodes(mocker):
 class TestIsGridDefinitionTemplate40:
     def test_true_for_valid_grid(self):
         cube = make_gaussian_cube()
-        assert save_rules.is_grid_definition_template_40(
+        assert is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
     def test_false_missing_attribute(self):
         cube = make_gaussian_cube(grib_grid_template=None)
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
     def test_false_wrong_attribute_value(self):
         cube = make_gaussian_cube(grib_grid_template=4)
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
     def test_false_attribute_not_int(self):
         cube = make_gaussian_cube()
         cube.attributes["GRIB2_GRID_TEMPLATE"] = "40"
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -106,14 +108,14 @@ class TestIsGridDefinitionTemplate40:
         cube.add_dim_coord(x_coord, 1)
         cube.add_aux_coord(y_coord, 0)
         cube.attributes["GRIB2_GRID_TEMPLATE"] = 40
-        assert not save_rules.is_grid_definition_template_40(cube, x_coord, y_coord)
+        assert not is_grid_definition_template_40(cube, x_coord, y_coord)
 
     def test_false_no_repeated_latitudes(self):
         cube = make_gaussian_cube(
             x_points=list(range(8)),
             y_points=[0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0],
         )
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -122,7 +124,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=list(range(8)),
             y_points=[-60.0, -60.0, -30.0, 0.0, -30.0, 30.0, 30.0, 60.0],
         )
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -131,7 +133,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=[0.0, 90.0, 0.0, 90.0, 0.0, 90.0],
             y_points=[-60.0, -60.0, 0.0, 0.0, 60.0, 60.0],
         )
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -140,7 +142,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=[0.0, 90.0, 180.0, 270.0, 0.0, 90.0, 180.0, 270.0],
             y_points=[-30.0, -30.0, 30.0, 30.0, -30.0, -30.0, 30.0, 30.0],
         )
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -149,7 +151,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=[0.0, 180.0, 90.0, 270.0, 0.0, 90.0, 180.0, 270.0],
             y_points=[-60.0, -60.0, -60.0, -60.0, 30.0, 30.0, 30.0, 30.0],
         )
-        assert not save_rules.is_grid_definition_template_40(
+        assert not is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -158,7 +160,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=[0.0, 90.0, 180.0, 270.0] * 4,
             y_points=[60.0] * 4 + [30.0] * 4 + [-30.0] * 4 + [-60.0] * 4,
         )
-        assert save_rules.is_grid_definition_template_40(
+        assert is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -167,7 +169,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=[270.0, 180.0, 90.0, 0.0] * 4,
             y_points=[-60.0] * 4 + [-30.0] * 4 + [30.0] * 4 + [60.0] * 4,
         )
-        assert save_rules.is_grid_definition_template_40(
+        assert is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -176,7 +178,7 @@ class TestIsGridDefinitionTemplate40:
             x_points=[0.0, 180.0, 0.0, 90.0, 180.0, 270.0],
             y_points=[-60.0, -60.0, 60.0, 60.0, 60.0, 60.0],
         )
-        assert save_rules.is_grid_definition_template_40(
+        assert is_grid_definition_template_40(
             cube, cube.coord("longitude"), cube.coord("latitude")
         )
 
@@ -186,9 +188,7 @@ class TestGridDefinitionTemplate40:
         cube = make_gaussian_cube()
         x_coord = cube.coord("longitude")
         y_coord = cube.coord("latitude")
-        returned = save_rules.grid_definition_template_40(
-            cube, mock_grib, x_coord, y_coord
-        )
+        returned = grid_definition_template_40(cube, mock_grib, x_coord, y_coord)
 
         assert returned is mock_grib
         assert mock_grib.keys["gridDefinitionTemplateNumber"] == 40
@@ -211,7 +211,7 @@ class TestGridDefinitionTemplate40:
             x_points=[270.0, 180.0, 90.0, 0.0] * 4,
             y_points=[-60.0] * 4 + [-30.0] * 4 + [30.0] * 4 + [60.0] * 4,
         )
-        save_rules.grid_definition_template_40(
+        grid_definition_template_40(
             cube, mock_grib, cube.coord("longitude"), cube.coord("latitude")
         )
         assert mock_grib.keys["longitudeOfFirstGridPoint"] == int(270.0e6)
@@ -222,7 +222,7 @@ class TestGridDefinitionTemplate40:
             x_points=[0.0, 90.0, 180.0, 270.0] * 4,
             y_points=[60.0] * 4 + [30.0] * 4 + [-30.0] * 4 + [-60.0] * 4,
         )
-        save_rules.grid_definition_template_40(
+        grid_definition_template_40(
             cube, mock_grib, cube.coord("longitude"), cube.coord("latitude")
         )
         assert mock_grib.keys["latitudeOfFirstGridPoint"] == int(60.0e6)
@@ -233,7 +233,7 @@ class TestGridDefinitionTemplate40:
             x_points=[0.0, 180.0, 0.0, 90.0, 180.0, 270.0],
             y_points=[-60.0, -60.0, 60.0, 60.0, 60.0, 60.0],
         )
-        save_rules.grid_definition_template_40(
+        grid_definition_template_40(
             cube, mock_grib, cube.coord("longitude"), cube.coord("latitude")
         )
         assert mock_grib.keys["N"] == 1
